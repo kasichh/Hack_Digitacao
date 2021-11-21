c71b4959ea56101a6126f4aa36d385ac9c2e3748
Kasich <pjesus192@gmail.com>
2021-11-21 16:52:27 -0300

    Segundo commit para teste igual ao primeiro

--- hack_digitacao.py
+++ hack_digitacao.py
@@ -10,3 +10,5 @@ driver.get('https://www.keybr.com/')
 sleep(2)
 driver.find_element(By.CLASS_NAME, 'X6jBH4QQC438WF7fZYzD').click()
 valores = driver.find_element(By.CLASS_NAME, 'pklLOFTYXcQTUpcTaOg7')
+
+
