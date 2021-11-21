d0f11d259b11bb6a243a24b0d2b030f6e8109660
Kasich <pjesus192@gmail.com>
2021-11-21 17:06:45 -0300

    arrumei a bagaca

--- hack_digitacao.py
+++ hack_digitacao.py
@@ -1,1 +1,13 @@
-print('Hello word!')
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+driver = webdriver.Chrome()
+
+driver.get('https://www.keybr.com/')
+sleep(2)
+driver.find_element(By.CLASS_NAME, 'X6jBH4QQC438WF7fZYzD').click()
+valores = driver.find_element(By.CLASS_NAME, 'pklLOFTYXcQTUpcTaOg7')
+
